656507dc1226b79e675790669f8a9d2bb5cdb522
Mayrapv <mayrarivaspv@gmail.com>
2023-03-29 20:03:13 -0300

    primer commit

---
+++ script/main.js
@@ -0,0 +1,15 @@
+alert ( "Hola, bienvenido a nuestro sistema de turnos Optica Eyra");
+
+let nombre = "Mayra";
+
+nombre = prompt ("¿Cual es tu nombre y apellido?");
+
+let edad = 14;
+
+if (edad == 18){
+
+alert("Tu puedes seleccionar un turno"
+);
+}else{
+    alert("Tu no puedes seleccionar un turno, eres menor de edad");
+}
