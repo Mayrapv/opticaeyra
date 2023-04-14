09909fd43549839c557fe0fccff2b3f6b97ec33e
Mayrapv <mayrarivaspv@gmail.com>
2023-04-14 10:46:17 -0300

    Agregamos listado de pacientes a atender

---
+++ readme.md
@@ -0,0 +1,10 @@
+Hola!
+Mi nombre es Mayra Rivas, yo vengo de cursar desarrollo web pero aun asi hay muchas cosas que me falta aprender, miro las clases las veces  que sean necesarias para poder aprender y entender cada ejemplo. 
+
+Los pongo al tanto de mi proyecto, voy a trabajar en un ecommerce para una optica donde la idea es tambien desarrollar un sistema de turnos medicos. 
+Mis ejemplos estan basados en esto mismo, espero haber trabajado bien en el codigo pero desde luego que sus correcciones me ayudan un monton a trabajar y aprender.
+
+Intente trabajar lo mas que pude con la consola, me cuesta bastante con los calculos, creo que es lo que mas me marea por el momento.
+
+Muchas gracias!
+Saludos, Mayra Rivas
--- script/main.js
+++ script/main.js
@@ -1,15 +1,38 @@
 alert ( "Hola, bienvenido a nuestro sistema de turnos Optica Eyra");
 
-let nombre = "Mayra";
+nombreapellido = prompt ("¿Cual es tu nombre y apellido?");
+console.log(nombreapellido);
 
-nombre = prompt ("¿Cual es tu nombre y apellido?");
+let edad = parseInt(prompt("¿Cual es tu edad?"));
+if (edad >= 18) {
 
-let edad = 14;
-
-if (edad == 18){
 
 alert("Tu puedes seleccionar un turno"
 );
 }else{
     alert("Tu no puedes seleccionar un turno, eres menor de edad");
 }
+
+
+let precio = 2500
+let pacientes
+let total
+pacientes = prompt ("¿Cuantas personas de tu grupo familiar se atenderan hoy?");
+console.log("--> Personas ingresadas", pacientes);
+
+if (pacientes) {
+    
+}
+/* LISTADO DE LOS 10 PACIENTES QUE SE ATENDERAN HOY CON EL DR*/
+for (let Nombreapellido ; <=10 ; index++) {
+    console.log("Nombre y apellido" ,Nombreapellido, "de las 10 personas que se atenderan hoy con el dr");
+    
+}
+
+
+
+
+
+
+
+
